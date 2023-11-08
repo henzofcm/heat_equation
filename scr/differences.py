One small calculation error fixed

scr/differences.py
@@ -5,6 +5,7 @@ LENGTH = 10
 TEMPERATURE_START = 100
 TOTAL_TIME = 10
 
+
 def create_difference_matrix():
     # Creates a temporary array of ones
     temp_array = np.ones(LENGTH)
@@ -17,36 +18,36 @@ def create_difference_matrix():
 
     return matrix
 
+
 def create_first_vector():
     # Creates the vector with constant temperature
     vector = TEMPERATURE_START * np.ones(LENGTH)
 
     return vector
 
+
 def find_solution(diff_matrix, first_vector, time):
-    # Self descriptive but finds the eigenvalues and eigenvectors
+    # Self descriptive: finds the eigenvalues and eigenvectors
     eigenvalues, eigenvectors = np.linalg.eig(diff_matrix)
-    
+
     # Prepares the output vector, the constants used and the e's
     output_vector = np.zeros(LENGTH)
     constants = np.dot(np.linalg.inv(eigenvectors), first_vector)
-    flow = np.exp(eigenvalues)**time
+    flow = np.exp(eigenvalues) ** time
 
     # Calculates each output state de facto
-    for index in np.arange(LENGTH):
-        # Just a holder for the dot product of the eigenvectors and eigenvalues
-        change = np.dot(eigenvectors[index, :].T, flow)
-
-        output_vector[index] += change * constants[index]
+    output_vector = np.dot(eigenvectors, flow * constants)
 
     return output_vector
+
 
 def plot_image():
     pass
 
+
 if __name__ == "__main__":
     D = create_difference_matrix()
     u = create_first_vector()
-
-    out = find_solution(D, u, 10)
+    
+    out = find_solution(D, u, 3)
     print(out)